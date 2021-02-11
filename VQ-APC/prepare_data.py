d550f419453079fdad6edcab0f802de193100f0a
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-11 11:50:41 +0100

    prepare data file

---
+++ VQ-APC/prepare_data.py
@@ -0,0 +1,45 @@
+import librosa
+import scipy
+import numpy as np
+
+
+
+def preemphasis(x, preemph):
+    return scipy.signal.lfilter([1, -preemph], [1], x)
+
+
+def mulaw_encode(x, mu):
+    mu = mu - 1
+    fx = np.sign(x) * np.log1p(mu * np.abs(x)) / np.log1p(mu)
+    return np.floor((fx + 1) / 2 * mu + 0.5)
+
+
+def mulaw_decode(y, mu):
+    mu = mu - 1
+    x = np.sign(y) / mu * ((1 + mu) ** np.abs(y) - 1)
+    return x
+
+
+def process_wav(wav_path, out_path, sr=160000, preemph=0.97, n_fft=2048, n_mels=80, hop_length=160,
+                win_length=400, fmin=50, top_db=80, bits=8, offset=0.0, duration=None):
+    wav, _ = librosa.load(wav_path, sr=sr,
+                          offset=offset, duration=duration)
+    wav = wav / np.abs(wav).max() * 0.999
+
+    mel = librosa.feature.melspectrogram(preemphasis(wav, preemph),
+                                         sr=sr,
+                                         n_fft=n_fft,
+                                         n_mels=n_mels,
+                                         hop_length=hop_length,
+                                         win_length=win_length,
+                                         fmin=fmin,
+                                         power=1)
+    logmel = librosa.amplitude_to_db(mel, top_db=top_db)
+    logmel = logmel / top_db + 1
+
+    # wav = mulaw_encode(wav, mu=2**bits)
+
+    # np.save(out_path + '.wav.npy', wav)
+    np.save(out_path + '.mel.npy', logmel)
+    # return out_path, logmel.shape[-1]
+    return logmel.shape[-1]
