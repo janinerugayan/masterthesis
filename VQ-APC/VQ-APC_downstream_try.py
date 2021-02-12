ea25a29fbe6012fd22cc5ec9320fc714e29186be
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-12 09:59:50 +0100

    lenghts as tensor

--- VQ-APC/VQ-APC_downstream_try.py
+++ VQ-APC/VQ-APC_downstream_try.py
@@ -129,10 +129,8 @@ with open('./preprocessed/lengths.pkl', 'rb') as f:
     lengths = pickle.load(f)
 seq_lengths_B = list(lengths.values())
 
-embed()
-
 frames_BxLxM = torch.load('./preprocessed/' + args.exp_name + '.pt')
-# seq_lengths_B = torch.as_tensor(lengths[args.exp_name + '.pt'], dtype=torch.int64, device=torch.device('cpu'))
+seq_lengths_B = torch.as_tensor(seq_lengths_B, dtype=torch.int64, device=torch.device('cpu'))
 testing = True
 
 predicted_BxLxM, hiddens_NxBxLxH, logits_NxBxLxC = pretrained_vqapc.module.forward(frames_BxLxM, seq_lengths_B, testing)
