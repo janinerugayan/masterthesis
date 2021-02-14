c15ca0967ff5e9882c86c91b47d209da45f60ae9
Janine Rugayan <janinelr@stud.ntnu.no>
2021-02-14 09:06:42 +0100

    record size of each iteration

--- VQ-APC/prepare_data.py
+++ VQ-APC/prepare_data.py
@@ -134,12 +134,12 @@ def prepare_torch_lengths_multiple(save_dir, logmel_path, max_seq_len, wav_id):
             filename = Path(file).stem
             data = np.load(logmel_path + file)
             for row in range(len(data)):
-                feature_vector = data[row]
-                log_mel.append([float(i) for i in feature_vector])
+                log_mel.append([float(i) for i in data[row]])
             id2len[filename + '.pt'] = min(len(log_mel), max_seq_len)
             log_mel = torch.FloatTensor(log_mel)  # convert 2D list to a pytorch as_tensor
             log_mel = F.pad(log_mel, (0, 0, 0, max_seq_len - log_mel.size(0))) # pad or truncate
-            torch.save(log_mel, os.path.join(save_dir, wav_id + '.pt'))
+            torch.save(log_mel, os.path.join(save_dir, filename + '.pt'))
+            print(f'file: {filename} torch size: {log_mel.size()}')
 
     with open(os.path.join(save_dir, 'lengths.pkl'), 'wb') as f:  # sequence lengths to be used for forward function?
         pickle.dump(id2len, f, protocol=4)
