fb3dcfaa1d7405365ee7e36c4f01f5aa7743caca
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-01 20:56:25 +0100

    try

--- VQ-APC/VQ-APC_downstream_try.py
+++ VQ-APC/VQ-APC_downstream_try.py
@@ -42,8 +42,8 @@ wav_path = args.sound_file
 export_dir_path = './preprocessed/'
 
 # randomly segment combined sound file
-min_len = 1000
-max_len = 5000
+min_len = 10000
+max_len = 40000
 randomseg(wav_path, export_dir_path, min_len, max_len)
 
 # process wav files to get log-mel feature vectors
@@ -58,7 +58,7 @@ process_wav_multiple(export_dir_path, output_path)
 
 wav_id = args.exp_name
 logmel_path = export_dir_path
-max_seq_len = 4000
+max_seq_len = 30000
 
 prepare_torch_lengths_multiple(logmel_path, max_seq_len, wav_id)
 
