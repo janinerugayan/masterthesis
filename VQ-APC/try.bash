ed9f16349a00f836e3eafe628231d098e0e4cdf4
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-01 11:13:15 +0100

    delete results for try run

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -23,6 +23,10 @@
 # rm -v *.wav *.npy *.pt
 # cd ..
 
+cd results/
+rm -v *.txt
+cd ..
+
 CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=combined_sounds_shuffled \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
 --pretrained_weights=./logs/feb-2_run1.dir/feb-2_run1__epoch_10.model \
