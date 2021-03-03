e038806462852cedaee30d8297e16ba85369a26d
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-03 12:25:48 +0100

    spacing

--- VQ-APC/VQ-APC_downstream_try.py
+++ VQ-APC/VQ-APC_downstream_try.py
@@ -135,8 +135,7 @@ for file in os.listdir(prevq_path):
 
 
 # read embedding matrix
-# embedding = rnn_outputs_BxLxH.squeeze().cpu().detach().numpy()
-# print(f'Embedding matrix shape: {embedding.shape}')
+
 # embedding_from_training = np.load(args.embedding)
 # embedding = embedding_from_training.squeeze()
 # print(f'Embedding matrix shape: {embedding.shape}')
