09efcd0a6df050ffded5c68a1cb145da12e6895b
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-04 18:37:52 +0100

    added deletion of previous run for testing

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -1,3 +1,10 @@
+cd preprocessed
+rm -rv 4-mar_combined_shuffled
+cd ..
+cd results
+rm -rv 4-mar_combined_shuffled
+cd ..
+
 CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=4-mar_combined_shuffled \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
 --pretrained_weights=./logs/mar-3_phoneseg.dir/mar-3_phoneseg__epoch_1.model \
