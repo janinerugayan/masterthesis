19ee2cb39defeca2d8844b0c64254e872d477c0f
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-05 09:36:35 +0100

    prevq append

--- VQ-APC/VQ-APC_downstream_try.py
+++ VQ-APC/VQ-APC_downstream_try.py
@@ -123,9 +123,8 @@ for file in os.listdir(logmel_path):
             frames_BxLxM = Variable(frames_BxLxM).cuda()
             lengths_B = Variable(lengths_B).cuda()
             __, features, __ = pretrained_vqapc.module.forward(frames_BxLxM, lengths_B, testing)
-            print(features)
-            prevq_rnn_outputs.append(features[-1, :, :, :])
 
+        prevq_rnn_outputs.append(features[-1, :, :, :])
         prevq = prevq_rnn_outputs.pop().squeeze().cpu().detach().numpy()
         print(np.shape(prevq))
 
