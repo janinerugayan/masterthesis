70f9925ff74a8e4a2cd716e6d48403c8a1a6a441
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-05 08:20:08 +0100

    mar-5 experiment

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -5,8 +5,8 @@ cd results
 rm -rv 4-mar_combined_shuffled
 cd ..
 
-CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=4-mar_combined_shuffled \
+CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=5-mar_phoneseg \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
---pretrained_weights=./logs/mar-3_phoneseg.dir/mar-3_phoneseg__epoch_1.model \
+--pretrained_weights=./logs/mar-5_training.dir/mar-5_training__epoch_10.model \
 --preprocess_path=./preprocessed/ \
 --out_path=./results/
