8afa591f38ca37b1983ef80f0493a4cb1a84d457
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-05 10:02:52 +0100

    try other model

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -7,6 +7,6 @@
 
 CUDA_VISIBLE_DEVICES=1 python VQ-APC_downstream_try.py --exp_name=5-mar_phoneseg \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
---pretrained_weights=./logs/mar-5_training.dir/mar-5_training__epoch_10.model \
+--pretrained_weights=./logs/mar-3_phoneseg.dir/mar-3_phoneseg__epoch_1.model \
 --preprocess_path=./preprocessed/ \
 --out_path=./results/
