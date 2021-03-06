4f54e28771f267431ceada75e2b33db33d8a15c1
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-06 18:08:15 +0100

    6-mar experiment

---
+++ VQ-APC/VQ-APC_split_wav.py
@@ -0,0 +1,44 @@
+from pydub import AudioSegment
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--preprocessed_wav_path', type=str)
+parser.add_argument('--wordseg_interval_dir', type=str)
+parser.add_argument('--output_dir', type=str)
+args = parser.parse_args()
+
+
+# load wordseg intervals
+wordseg_interval_dict = {}
+wordseg_dir = args.wordseg_interval_dir
+print('Reading: {}'.format(wordseg_dir))
+for file in os.listdir(wordseg_dir):
+    fn = Path(file).stem
+    wordseg_interval_dict[fn] = []
+    f = open(wordseg_dir + file, 'r')
+    for line in f:
+        if len(line) == 0
+            wordseg_interval_dict.pop(fn)
+            continue
+        start, end, label = line.split()
+        start = int(start)
+        end = int(end)
+        wordseg_interval_dict[fn].append((start, end))
+    f.close()
+
+utterances = wordseg_interval_dict.keys()
+
+# segment the combined wav file
+wav_path = args.preprocessed_wav_path
+export_dir = args.output_dir + '/wavs/'
+os.makedirs(export_dir)
+for utt_key in utterances:
+    wav_original = AudioSegment.from_wav(wav_path + utt_key + '.wav')
+    total_len = len(wav_original)
+    count = 0
+    for (word_start, word_end) in wordseg_interval_dict[utt_key]:
+        wav_segment = wav_original[word_start : word_end]
+        wav_segment.export(export_dir + 'word' + str(count) + '.wav', format='wav')
+        count += 1
--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -1,13 +1,6 @@
-# cd preprocessed
-# rm -rv 4-mar_combined_shuffled
-# cd ..
-# cd results
-# rm -rv 4-mar_combined_shuffled
-# cd ..
-
 eval "$(conda shell.bash hook)"
+
 conda activate vq-apc
-# # CUDA_VISIBLE_DEVICES=1
 python VQ-APC_downstream_try.py --exp_name=6-mar_phoneseg \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
 --pretrained_weights=./logs/mar-5_training_run2.dir/mar-5_training_run2__epoch_5.model \
@@ -18,4 +11,10 @@ conda activate wordseg
 
 python VQ-APC_word_seg.py --wordseg_algorithm=ag \
 --phoneseg_interval_dir=./results/6-mar_phoneseg/intervals/ \
+--output_dir=./results/6-mar_wordseg
+
+conda activate vq-apc
+
+python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/6-mar_phoneseg \
+--wordseg_interval_dir=./results/6-mar_wordseg \
 --output_dir=./results/6-mar_wordseg
