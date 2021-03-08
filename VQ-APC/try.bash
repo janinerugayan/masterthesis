018a4c4c6da3fd7b848c2c0c46e103a66095f71e
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-08 12:31:32 +0100

    8-mar experiment

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -1,7 +1,7 @@
 eval "$(conda shell.bash hook)"
 
 conda activate vq-apc
-python VQ-APC_downstream_try.py --exp_name=7-mar_phoneseg \
+python VQ-APC_downstream_try.py --exp_name=8-mar_phoneseg \
 --sound_file=./wavs/combined_sounds_shuffled.wav \
 --pretrained_weights=./logs/mar-5_training_run2.dir/mar-5_training_run2__epoch_5.model \
 --preprocess_path=./preprocessed/ \
@@ -10,14 +10,14 @@ python VQ-APC_downstream_try.py --exp_name=7-mar_phoneseg \
 conda activate wordseg
 
 python VQ-APC_word_seg.py --wordseg_algorithm=ag \
---phoneseg_interval_dir=./results/7-mar_phoneseg/intervals/ \
---output_dir=./results/7-mar_wordseg
+--phoneseg_interval_dir=./results/8-mar_phoneseg/intervals/ \
+--output_dir=./results/8-mar_wordseg
 
 conda activate vq-apc
 
-python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/7-mar_phoneseg/ \
---wordseg_interval_dir=./results/7-mar_wordseg/intervals/ \
---output_dir=./results/7-mar_wordseg
+python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/8-mar_phoneseg/ \
+--wordseg_interval_dir=./results/8-mar_wordseg/intervals/ \
+--output_dir=./results/8-mar_wordseg
 
-python VQ-APC_stt_recog.py --wav_path=./results/7-mar_wordseg/wavs \
---output_dir=./results/7-mar_wordseg
+python VQ-APC_stt_recog.py --wav_path=./results/8-mar_wordseg/wavs \
+--output_dir=./results/8-mar_wordseg
