a4c90107ed7206d207040b6536da8fed933a7451
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-10 13:36:16 +0100

    train 100 epochs

--- VQ-APC/train.bash
+++ VQ-APC/train.bash
@@ -1,4 +1,4 @@
-# CUDA_VISIBLE_DEVICES=1 
+# CUDA_VISIBLE_DEVICES=1
 python train_vqapc.py --rnn_num_layers 3 \
                       --rnn_hidden_size 512 \
                       --rnn_dropout 0.1 \
@@ -17,5 +17,5 @@ python train_vqapc.py --rnn_num_layers 3 \
                       --train_sampling 1. \
                       --val_partition dev-clean \
                       --val_sampling 1. \
-                      --exp_name mar-9_training_10epochs \
+                      --exp_name mar-10_training_100epochs \
                       --store_path ./logs
