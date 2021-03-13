321201d13a47d96598a2a31a54c4c5e3a0344176
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-13 14:19:35 +0100

    yml file and 100 epoch training using both GPUs

--- VQ-APC/train.bash
+++ VQ-APC/train.bash
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=1 python train_vqapc.py --rnn_num_layers 3 \
+# CUDA_VISIBLE_DEVICES=1
+python train_vqapc.py --rnn_num_layers 3 \
                       --rnn_hidden_size 512 \
                       --rnn_dropout 0.1 \
                       --rnn_residual \
@@ -9,7 +10,7 @@ CUDA_VISIBLE_DEVICES=1 python train_vqapc.py --rnn_num_layers 3 \
                       --optimizer adam \
                       --batch_size 32  \
                       --learning_rate 0.0001 \
-                      --epochs 100 \
+                      --epochs 1000 \
                       --n_future 5 \
                       --librispeech_home ./librispeech_data/preprocessed \
                       --train_partition train-clean-360 \
