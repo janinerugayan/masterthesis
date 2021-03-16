7eadc990a05e4b79f109a69956978b36d4901100
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-16 11:17:15 +0100

    16-mar experiment with 100 epoch model

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -1,14 +1,14 @@
 # cd preprocessed
 # rm -rv 14-mar_check*
-cd results
-rm -rv 14-mar_check*
-cd ..
+# cd results
+# rm -rv 14-mar_check*
+# cd ..
 
 eval "$(conda shell.bash hook)"
 
 conda activate vq-apc
 
-python VQ-APC_downstream_try.py --exp_name=14-mar_check_phoneseg \
+python VQ-APC_downstream_try.py --exp_name=16-mar_check_phoneseg \
 --sound_file=./wavs/numbers_shuffled.wav \
 --pretrained_weights=./logs/mar-13_training_100epochs.dir/mar-13_training_100epochs__epoch_100.model \
 --preprocess_path=./preprocessed/ \
@@ -17,14 +17,14 @@ python VQ-APC_downstream_try.py --exp_name=14-mar_check_phoneseg \
 conda activate wordseg
 
 python VQ-APC_word_seg.py --wordseg_algorithm=ag \
---phoneseg_interval_dir=./results/14-mar_check_phoneseg/intervals/ \
---output_dir=./results/14-mar_check_wordseg_ag
+--phoneseg_interval_dir=./results/16-mar_check_phoneseg/intervals/ \
+--output_dir=./results/16-mar_check_wordseg_ag
 
 conda activate vq-apc
 
-python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/14-mar_check_phoneseg/ \
---wordseg_interval_dir=./results/14-mar_check_wordseg_ag/intervals/ \
---output_dir=./results/14-mar_check_wordseg_ag
+python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/16-mar_check_phoneseg/ \
+--wordseg_interval_dir=./results/16-mar_check_wordseg_ag/intervals/ \
+--output_dir=./results/16-mar_check_wordseg_ag
 
-python VQ-APC_stt_recog.py --wav_path=./results/14-mar_check_wordseg_ag/wavs \
---output_dir=./results/14-mar_check_wordseg_ag
+python VQ-APC_stt_recog.py --wav_path=./results/16-mar_check_wordseg_ag/wavs \
+--output_dir=./results/16-mar_check_wordseg_ag
