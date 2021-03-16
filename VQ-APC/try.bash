2552d22de53193aaed3f1d557876f223f04ccb9a
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-16 19:40:55 +0100

    1 utterance experiment

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -6,25 +6,25 @@
 
 eval "$(conda shell.bash hook)"
 
-# conda activate vq-apc
+conda activate vq-apc
 
-# python VQ-APC_downstream_try.py --exp_name=16-mar_check_phoneseg \
-# --sound_file=./wavs/numbers_shuffled.wav \
-# --pretrained_weights=./logs/mar-13_training_100epochs.dir/mar-13_training_100epochs__epoch_100.model \
-# --preprocess_path=./preprocessed/ \
-# --out_path=./results/
+python VQ-APC_downstream_try.py --exp_name=16-mar_1utt_phoneseg \
+--sound_file=./wavs/numbers_shuffled.wav \
+--pretrained_weights=./logs/mar-13_training_100epochs.dir/mar-13_training_100epochs__epoch_100.model \
+--preprocess_path=./preprocessed/ \
+--out_path=./results/
 
 conda activate wordseg
 
 python VQ-APC_word_seg.py --wordseg_algorithm=ag \
---phoneseg_interval_dir=./results/16-mar_check_phoneseg/intervals/ \
---output_dir=./results/16-mar_check_wordseg_ag
+--phoneseg_interval_dir=./results/16-mar_1utt_phoneseg/intervals/ \
+--output_dir=./results/16-mar_1utt_wordseg_ag
 
 conda activate vq-apc
 
-python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/16-mar_check_phoneseg/ \
---wordseg_interval_dir=./results/16-mar_check_wordseg_ag/intervals/ \
---output_dir=./results/16-mar_check_wordseg_ag
+python VQ-APC_split_wav.py --preprocessed_wav_path=./preprocessed/16-mar_1utt_phoneseg/ \
+--wordseg_interval_dir=./results/16-mar_1utt_wordseg_ag/intervals/ \
+--output_dir=./results/16-mar_1utt_wordseg_ag
 
-python VQ-APC_stt_recog.py --wav_path=./results/16-mar_check_wordseg_ag/wavs \
---output_dir=./results/16-mar_check_wordseg_ag
+python VQ-APC_stt_recog.py --wav_path=./results/16-mar_1utt_wordseg_ag/wavs \
+--output_dir=./results/16-mar_1utt_wordseg_ag
