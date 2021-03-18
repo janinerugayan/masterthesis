b41ba6ad36efe6057502ec84dc92cf851db2c716
Janine Rugayan <janinelr@stud.ntnu.no>
2021-03-18 12:05:39 +0100

    import embed

--- VQ-APC/phoneseg_algorithms.py
+++ VQ-APC/phoneseg_algorithms.py
@@ -11,7 +11,7 @@ from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 
 import pandas as pd
-
+from IPython import embed
 
 #-----------------------------------------------------------------------------#
 #                             GREEDY SEGMENTATION                             #
