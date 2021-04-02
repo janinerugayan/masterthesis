98d6c77c197954414b609afbfefad9ca76b82d3d
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-02 09:57:08 +0200

    removed logits

--- VQ-APC/VQ-APC_downstream_try.py
+++ VQ-APC/VQ-APC_downstream_try.py
@@ -149,8 +149,6 @@ for file in os.listdir(logmel_path):
         logits_file = output_dir + filename + '_logits.csv'
         df_logits = pd.DataFrame(logits)
         df_logits.to_csv(logits_file, index=True, header=False, mode='w')
-        with open(output_dir + filename + '_logits.txt', 'w') as file:
-            np.savetxt(file, logits, fmt='%.16f')
 
 
 
@@ -162,7 +160,7 @@ for file in os.listdir(logmel_path):
 prevq_path = args.out_path + args.exp_name + '/prevq/'
 prevq_dict = {}
 for file in os.listdir(prevq_path):
-    if file.endswith('logits.txt'):
+    if file.endswith('.txt'):
         filename = Path(file).stem
         print(f'Reading pre-quantisation for {file}')
         prevq_dict[filename] = np.loadtxt(prevq_path + file)
