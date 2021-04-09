13fe41251475adc369e5ed622c98765e2213996f
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-09 19:32:47 +0200

    corrected

--- LBG/lbg.bash
+++ LBG/lbg.bash
@@ -3,6 +3,6 @@ CUDA_VISIBLE_DEVICES=1 python train_LBG.py --batch_size 32  \
                       --train_partition train-clean-360 \
                       --train_sampling 1. \
                       --val_partition dev-clean \
-                      --val_sampling 1. \
-                      --exp_name mar-31_codesize128_1000epochs \
-                      --store_path ./logs
+                      --val_sampling 1. 
+                      # --exp_name mar-31_codesize128_1000epochs \
+                      # --store_path ./logs
