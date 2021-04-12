1d9a0511f1acc3d81b4c21f834d8baac9c09cb0a
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-12 12:27:57 +0200

    removed printing of prepared text

--- VQ-APC/VQ-APC_word_seg.py
+++ VQ-APC/VQ-APC_word_seg.py
@@ -59,7 +59,6 @@ for utt_key in utterances:
     prepared_text.append(
         " ".join([i[2] + "_" for i in phoneseg_interval_dict[utt_key]])
         )
-print(prepared_text[:10])
 word_segmentation = segment_func(prepared_text, **kwargs)
 # print(prepared_text[:10])
 wordseg_interval_dict = {}
