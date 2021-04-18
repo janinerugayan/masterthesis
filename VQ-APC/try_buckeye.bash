77ccbf4f5fa15c0038c7d30ab4c3e964cd1babd2
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-18 20:15:16 +0200

    corrected

--- VQ-APC/try_buckeye.bash
+++ VQ-APC/try_buckeye.bash
@@ -6,7 +6,7 @@ conda activate vq-apc
 
 python VQ-APC_downstream_buckeye.py --exp_name=$1 \
 --sound_file=./buckeye_data/so_we_can_see.wav \
---in_path=./buckeyey_data/ \
+--in_path=./buckeye_data/ \
 --pretrained_weights=./logs/mar-30_codesize512_lr10-4_101-1000epochs.dir/mar-30_codesize512_lr10-4_101-1000epochs__epoch_1000.model \
 --preprocess_path=./preprocessed/ \
 --out_path=./results/ \
