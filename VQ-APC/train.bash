e1ccc3531ad97409f3e263f07fbb4c7671b1b0fc
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-18 20:03:08 +0200

    changed lr to 10-5

--- VQ-APC/train.bash
+++ VQ-APC/train.bash
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=1 python train_vqapc.py --rnn_num_layers 3 \
                       --apply_VQ 0 0 1 \
                       --optimizer adam \
                       --batch_size 32  \
-                      --learning_rate 0.001 \
+                      --learning_rate 0.00001 \
                       --epochs 2000 \
                       --n_future 5 \
                       --librispeech_home ./librispeech_data/preprocessed \
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=1 python train_vqapc.py --rnn_num_layers 3 \
                       --train_sampling 1. \
                       --val_partition dev-clean \
                       --val_sampling 1. \
-                      --exp_name apr-18_codesize128_2000epochs \
+                      --exp_name apr-18_codesize128_2000epochs_lr10-5 \
                       --store_path ./logs \
                       --checkpoint_model ./logs/mar-31_codesize128_1000epochs.dir/mar-31_codesize128_1000epochs__epoch_1000.model \
                       --checkpoint_epoch 1000
