f96ce50de2a49bcf477a911aaede47128aa0ff8a
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-21 09:51:21 +0200

    reuse wav segments to try 1utt with dw=9

--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -5,7 +5,7 @@ eval "$(conda shell.bash hook)"
 conda activate vq-apc
 
 python VQ-APC_downstream_try.py --exp_name=$1 \
---sound_file=./wavs/1utt_numbers_clean.wav \
+--sound_file=./wavs/numbers_shuffled.wav \
 --pretrained_weights=./logs/mar-30_codesize512_lr10-4_101-1000epochs.dir/mar-30_codesize512_lr10-4_101-1000epochs__epoch_1000.model \
 --preprocess_path=./preprocessed/ \
 --out_path=./results/ \
