e0565c3476232c43d97032c328eca585bc1231e0
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-27 20:42:48 +0200

    added dw=40

--- VQ-APC/number_shuffled_tp.bash
+++ VQ-APC/number_shuffled_tp.bash
@@ -3,3 +3,5 @@ bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw16 tp 16
 bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw25 tp 25
 
 bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw36 tp 36
+
+bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw40 tp 40
