ce6f691825bdf768722dfc7c72605001228248dc
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-27 20:42:15 +0200

    revert to shuffled wav, bash script for experiment

---
+++ VQ-APC/number_shuffled_tp.bash
@@ -0,0 +1,5 @@
+bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw16 tp 16
+
+bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw25 tp 25
+
+bash try.bash apr-27_shuffled10sec_epoch1108_tp_dw36 tp 36
