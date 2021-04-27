408a9853ee409513db97c0ac356a9b592814afc2
Janine Rugayan <janinelr@stud.ntnu.no>
2021-04-27 22:50:29 +0200

    new experiment with epoch 1518

---
+++ VQ-APC/experiment.bash
@@ -0,0 +1,15 @@
+bash try.bash apr-27_shuffled10sec_epoch1518_ag_dw16 ag 16
+
+bash try.bash apr-27_shuffled10sec_epoch1518_ag_dw25 ag 25
+
+bash try.bash apr-27_shuffled10sec_epoch1518_ag_dw36 ag 36
+
+bash try.bash apr-27_shuffled10sec_epoch1518_ag_dw40 ag 40
+
+bash try.bash apr-27_shuffled10sec_epoch1518_tp_dw16 tp 16
+
+bash try.bash apr-27_shuffled10sec_epoch1518_tp_dw25 tp 25
+
+bash try.bash apr-27_shuffled10sec_epoch1518_tp_dw36 tp 36
+
+bash try.bash apr-27_shuffled10sec_epoch1518_tp_dw40 tp 40
