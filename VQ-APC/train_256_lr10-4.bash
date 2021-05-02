76e19952e4eca233d915fd8b00bb52a75871610c
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-02 20:09:44 +0200

    train codebook size 256

---
+++ VQ-APC/train_256_lr10-4.bash
@@ -0,0 +1,20 @@
+python train_vqapc.py --rnn_num_layers 3 \
+                      --rnn_hidden_size 512 \
+                      --rnn_dropout 0.1 \
+                      --rnn_residual \
+                      --codebook_size 256 \
+                      --code_dim 512 \
+                      --gumbel_temperature 0.5 \
+                      --apply_VQ 0 0 1 \
+                      --optimizer adam \
+                      --batch_size 32  \
+                      --learning_rate 0.0001 \
+                      --epochs 2000 \
+                      --n_future 5 \
+                      --librispeech_home ./librispeech_data/preprocessed \
+                      --train_partition train-clean-360 \
+                      --train_sampling 1. \
+                      --val_partition dev-clean \
+                      --val_sampling 1. \
+                      --exp_name may-2_codesize256_lr10-4_2000epochs \
+                      --store_path ./logs \
