52fffed9d006c58fb207d151f6e880b192aa69d3
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-10 11:03:36 +0200

    epoch 2000 experiment

--- VQ-APC/experiment.bash
+++ VQ-APC/experiment.bash
@@ -1,11 +1,11 @@
+bash try.bash may-10_numshuffled_epoch2000_maxframe100_mod-ag_dw9 ag 9 100
+bash try.bash may-10_numshuffled_epoch2000_maxframe100_mod-ag_dw16 ag 16 100
+bash try.bash may-10_numshuffled_epoch2000_maxframe100_mod-ag_dw25 ag 25 100
+bash try.bash may-10_numshuffled_epoch2000_maxframe100_mod-ag_dw36 ag 36 100
+bash try.bash may-10_numshuffled_epoch2000_maxframe100_mod-ag_dw40 ag 40 100
 
-bash try.bash may-6_actionwords_epoch1000_maxframe100_mod-ag_dw16 ag 16 100
-bash try.bash may-6_actionwords_epoch1000_maxframe100_mod-ag_dw25 ag 25 100
-bash try.bash may-6_actionwords_epoch1000_maxframe100_mod-ag_dw36 ag 36 100
-bash try.bash may-6_actionwords_epoch1000_maxframe100_mod-ag_dw40 ag 40 100
-
-bash try.bash may-6_actionwords_epoch1000_maxframe150_mod-ag_dw9 ag 9 150
-bash try.bash may-6_actionwords_epoch1000_maxframe150_mod-ag_dw16 ag 16 150
-bash try.bash may-6_actionwords_epoch1000_maxframe150_mod-ag_dw25 ag 25 150
-bash try.bash may-6_actionwords_epoch1000_maxframe150_mod-ag_dw36 ag 36 150
-bash try.bash may-6_actionwords_epoch1000_maxframe150_mod-ag_dw40 ag 40 150
+bash try.bash may-10_numshuffled_epoch2000_maxframe150_mod-ag_dw9 ag 9 150
+bash try.bash may-10_numshuffled_epoch2000_maxframe150_mod-ag_dw16 ag 16 150
+bash try.bash may-10_numshuffled_epoch2000_maxframe150_mod-ag_dw25 ag 25 150
+bash try.bash may-10_numshuffled_epoch2000_maxframe150_mod-ag_dw36 ag 36 150
+bash try.bash may-10_numshuffled_epoch2000_maxframe150_mod-ag_dw40 ag 40 150
--- VQ-APC/try.bash
+++ VQ-APC/try.bash
@@ -5,8 +5,8 @@ eval "$(conda shell.bash hook)"
 conda activate vq-apc
 
 python VQ-APC_downstream_try.py --exp_name=$1 \
---sound_file=./wavs/combined_sounds_shuffled.wav \
---pretrained_weights=./logs/mar-30_codesize512_lr10-4_101-1000epochs.dir/mar-30_codesize512_lr10-4_101-1000epochs__epoch_1000.model \
+--sound_file=./wavs/numbers_shuffled.wav \
+--pretrained_weights=./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model \
 --preprocess_path=./preprocessed/ \
 --out_path=./results/ \
 --codebook_size=512 \
