ac4df3d9729448f5cf0c4ed640b5bdddffcd1deb
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-11 09:03:46 +0200

    tp experiment with epoch 1518

--- VQ-APC/experiment.bash
+++ VQ-APC/experiment.bash
@@ -1,11 +1,11 @@
-bash try.bash may-10_numshuffled_epoch1518_maxframe100_mod-ag_dw9 ag 9 100
-bash try.bash may-10_numshuffled_epoch1518_maxframe100_mod-ag_dw16 ag 16 100
-bash try.bash may-10_numshuffled_epoch1518_maxframe100_mod-ag_dw25 ag 25 100
-bash try.bash may-10_numshuffled_epoch1518_maxframe100_mod-ag_dw36 ag 36 100
-bash try.bash may-10_numshuffled_epoch1518_maxframe100_mod-ag_dw40 ag 40 100
+bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw9 tp 9 100
+bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw16 tp 16 100
+bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw25 tp 25 100
+bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw36 tp 36 100
+bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw40 tp 40 100
 
-bash try.bash may-10_numshuffled_epoch1518_maxframe150_mod-ag_dw9 ag 9 150
-bash try.bash may-10_numshuffled_epoch1518_maxframe150_mod-ag_dw16 ag 16 150
-bash try.bash may-10_numshuffled_epoch1518_maxframe150_mod-ag_dw25 ag 25 150
-bash try.bash may-10_numshuffled_epoch1518_maxframe150_mod-ag_dw36 ag 36 150
-bash try.bash may-10_numshuffled_epoch1518_maxframe150_mod-ag_dw40 ag 40 150
+bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw9 tp 9 150
+bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw16 tp 16 150
+bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw25 tp 25 150
+bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw36 tp 36 150
+bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw40 tp 40 150
