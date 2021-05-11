bc092db4cfa95ce1a94ab8e1496f662a7666257f
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-11 11:11:43 +0200

    epoch 2000 experiment with wordseg TP

--- VQ-APC/experiment.bash
+++ VQ-APC/experiment.bash
@@ -1,11 +1,11 @@
-bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw9 tp 9 100
-bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw16 tp 16 100
-bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw25 tp 25 100
-bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw36 tp 36 100
-bash try.bash may-11_numshuffled_epoch1518_maxframe100_dw40 tp 40 100
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe100_dw9 tp 9 100
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe100_dw16 tp 16 100
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe100_dw25 tp 25 100
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe100_dw36 tp 36 100
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe100_dw40 tp 40 100
 
-bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw9 tp 9 150
-bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw16 tp 16 150
-bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw25 tp 25 150
-bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw36 tp 36 150
-bash try.bash may-11_numshuffled_epoch1518_maxframe150_dw40 tp 40 150
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe150_dw9 tp 9 150
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe150_dw16 tp 16 150
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe150_dw25 tp 25 150
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe150_dw36 tp 36 150
+bash try.bash may-11_numshuffled_epoch2000_TP_maxframe150_dw40 tp 40 150
