e57c8ab3024e5c264eef75e1b777ad836446860e
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-12 14:16:04 +0200

    time elapsed also reported

--- VQ-APC/experiments/VQ_spolacq_size512.bash
+++ VQ-APC/experiments/VQ_spolacq_size512.bash
@@ -1,8 +1,8 @@
 cd ..
 
-bash VQ-APC_segmentation.bash may-12_size512epoch2000 ag 36 100 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model
+bash VQ-APC_segmentation.bash may-12_size512epoch2000 ag 36 100 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model >/dev/null
 
 cd ../spolacq_mod/scripts/
 
 python main.py --data_name=may-12_VQ-spolacq_size512epoch2000 \
---recog_dict=../../VQ-APC/results/may-12_size512epoch2000_ag/recog_results_dict.pkl
+--recog_dict=../../VQ-APC/results/may-12_size512epoch2000_ag/recog_results_dict.pkl >/dev/null
