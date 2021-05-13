1995d777e6040b1edf0ca0a3804419e1e6f66b59
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-13 10:06:39 +0200

    changing code size exp with spolacq, using wordseg-tp

--- VQ-APC/experiments/VQ_spolacq_size128.bash
+++ VQ-APC/experiments/VQ_spolacq_size128.bash
@@ -1,8 +1,8 @@
 cd ..
 
-bash segmentation.bash may-12_size128epoch2000 ag 36 100 128 ./logs/apr-19_codesize128_1208-2000epochs.dir/apr-19_codesize128_1208-2000epochs__epoch_2000.model >/dev/null
+bash segmentation.bash may-13_size128epoch2000 tp 36 100 128 ./logs/apr-19_codesize128_1208-2000epochs.dir/apr-19_codesize128_1208-2000epochs__epoch_2000.model >/dev/null
 
 cd ../spolacq_mod/scripts/
 
-python main.py --data_name=may-12_VQ-spolacq_size128epoch2000 \
---recog_dict=../../VQ-APC/results/may-12_size128epoch2000_ag/recog_results_dict.pkl >/dev/null
+python main.py --data_name=may-13_VQ-spolacq_size128epoch2000 \
+--recog_dict=../../VQ-APC/results/may-13_size128epoch2000_tp/recog_results_dict.pkl >/dev/null
