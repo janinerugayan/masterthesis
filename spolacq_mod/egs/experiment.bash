93f1f06e88f562604003d40dfa2c3e71bc71ad49
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-13 13:01:10 +0200

    experiment setup for simulated wordseg results

--- spolacq_mod/egs/experiment.bash
+++ spolacq_mod/egs/experiment.bash
@@ -1,24 +1,13 @@
 cd ../scripts/
 
-python main.py --data_name=may-4_wordrecogexp_dw25-run2_mean-var-norm
+python main_simulate_wordseg.py --data_name= may-13_simulatedwordseg_10percent \
+--simulated_wordseg={'num_words': 500, 'zero':5, 'one':5, 'two':5, 'three':5, 'four':5, 'five':5, 'six':5, 'seven':5, 'eight':5, 'nine':5}
 
-# echo "========= START RL for 05-Dec dictionary ========="
-# python main.py --data_name=recog_results_05-Dec_SHUFFLED_run1
-# python main.py --data_name=recog_results_05-Dec_SHUFFLED_run2
-# python main.py --data_name=recog_results_05-Dec_SHUFFLED_run3
-# echo "========= RL - SHUFFLED - done ========="
+python main_simulate_wordseg.py --data_name= may-13_simulatedwordseg_20percent \
+--simulated_wordseg={'num_words': 500, 'zero':10, 'one':10, 'two':10, 'three':10, 'four':10, 'five':10, 'six':10, 'seven':10, 'eight':10, 'nine':10}
 
-# echo "========= START RL for 05-Dec dictionary ========="
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run1
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run2
-# python main.py --data_name=recog_results_05-Dec_notshuffled_run3
-# echo "========= RL - not shuffled - done ========="
+python main_simulate_wordseg.py --data_name= may-13_simulatedwordseg_30percent \
+--simulated_wordseg={'num_words': 500, 'zero':15, 'one':15, 'two':15, 'three':15, 'four':15, 'five':15, 'six':15, 'seven':15, 'eight':15, 'nine':15}
 
-#
-# echo "ALL DONE"
-
-# echo "========= START RL for 07-Dec dictionary ========="
-# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run1
-# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run2
-# python main.py --data_name=recog_results_07-Dec_notshuffled_randomseg_run3
-# echo "========= RL - Not shuffled - Randomseg - done ========="
+python main_simulate_wordseg.py --data_name= may-13_simulatedwordseg_40percent \
+--simulated_wordseg={'num_words': 500, 'zero':20, 'one':20, 'two':20, 'three':20, 'four':20, 'five':20, 'six':20, 'seven':20, 'eight':20, 'nine':20}
