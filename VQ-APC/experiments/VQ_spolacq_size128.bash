2b618021a6be92c01bb7ca6a169f0daf77b13d0f
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-13 16:11:14 +0200

    removed dev/null

--- VQ-APC/experiments/VQ_spolacq_size128.bash
+++ VQ-APC/experiments/VQ_spolacq_size128.bash
@@ -1,8 +1,8 @@
 cd ..
 
-bash segmentation.bash may-13_size128epoch2000 tp 36 100 128 ./logs/apr-19_codesize128_1208-2000epochs.dir/apr-19_codesize128_1208-2000epochs__epoch_2000.model >/dev/null
+bash segmentation.bash may-13_size128epoch2000 tp 36 100 128 ./logs/apr-19_codesize128_1208-2000epochs.dir/apr-19_codesize128_1208-2000epochs__epoch_2000.model
 
 cd ../spolacq_mod/scripts/
 
 python main.py --data_name=may-13_VQ-spolacq_size128epoch2000 \
---recog_dict=../../VQ-APC/results/may-13_size128epoch2000_tp/recog_results_dict.pkl >/dev/null
+--recog_dict=../../VQ-APC/results/may-13_size128epoch2000_tp/recog_results_dict.pkl
--- VQ-APC/experiments/VQ_spolacq_size512.bash
+++ VQ-APC/experiments/VQ_spolacq_size512.bash
@@ -1,8 +1,8 @@
 cd ..
 
-bash segmentation.bash may-13_size512epoch2000 tp 36 100 512 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model >/dev/null
+bash segmentation.bash may-13_size512epoch2000 tp 36 100 512 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model
 
 cd ../spolacq_mod/scripts/
 
 python main.py --data_name=may-13_VQ-spolacq_size512epoch2000 \
---recog_dict=../../VQ-APC/results/may-13_size512epoch2000_tp/recog_results_dict.pkl >/dev/null
+--recog_dict=../../VQ-APC/results/may-13_size512epoch2000_tp/recog_results_dict.pkl
