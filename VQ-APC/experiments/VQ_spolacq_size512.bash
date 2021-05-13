98f40044a1219d4595e2405f4f7c7285351d5a38
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-13 17:39:43 +0200

    run 3 of changing code size experiment

--- VQ-APC/experiments/VQ_spolacq_size512.bash
+++ VQ-APC/experiments/VQ_spolacq_size512.bash
@@ -2,11 +2,11 @@ eval "$(conda shell.bash hook)"
 
 cd ..
 
-bash segmentation.bash may-13_size512epoch2000_ag_run2 ag 36 100 512 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model
+bash segmentation.bash may-13_size512epoch2000_ag_run3 ag 36 100 512 ./logs/apr-17_codesize512_lr10-4_2000epochs.dir/apr-17_codesize512_lr10-4_2000epochs__epoch_2000.model
 
 conda activate spolacq
 
 cd ../spolacq_mod/scripts/
 
-python main.py --data_name=may-13_VQ-spolacq_size512epoch2000_ag_run2 \
---recog_dict=../../VQ-APC/results/may-13_size512epoch2000_ag_run2_ag/recog_results_dict.pkl
+python main.py --data_name=may-13_VQ-spolacq_size512epoch2000_ag_run3 \
+--recog_dict=../../VQ-APC/results/may-13_size512epoch2000_ag_run3_ag/recog_results_dict.pkl
