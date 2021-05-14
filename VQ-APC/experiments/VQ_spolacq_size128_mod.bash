1d4ecdc7878c54bfe8b5b49e29e39754ec602a30
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-14 19:37:31 +0200

    redoing all experiments with 5 runs each

--- VQ-APC/experiments/VQ_spolacq_size128_mod.bash
+++ VQ-APC/experiments/VQ_spolacq_size128_mod.bash
@@ -1,4 +1,4 @@
-# $1 = run number, $2 = wordseg algo, $3 = dur_weight
+# $1 = run number, $2 = wordseg algo, $3 = dur_weight, $4 = data name
 
 eval "$(conda shell.bash hook)"
 
@@ -10,5 +10,5 @@ conda activate spolacq
 
 cd ../spolacq_mod/scripts/
 
-python main.py --data_name=$4_$2_run$1 \
+python main.py --data_name=$4_$2_dw$3_run$1 \
 --recog_dict=../../VQ-APC/results/$4_$2_dw$3_run$1_$2/recog_results_dict.pkl
--- VQ-APC/experiments/run_mod.bash
+++ VQ-APC/experiments/run_mod.bash
@@ -1,7 +1,40 @@
-for i in {2..3}; do
-  bash VQ_spolacq_size128_mod.bash $i tp 36 may-14_size128epoch2000
+# code size 128
+
+# using wordseg-ag
+for i in {1..5}; do
+  bash VQ_spolacq_size128_mod.bash $i ag 25 may-15_size128epoch2000
 done
 
-for i in {2..3}; do
-  bash VQ_spolacq_size512_mod.bash $i tp 36 may-14_size512epoch2000
+for i in {1..5}; do
+  bash VQ_spolacq_size128_mod.bash $i ag 36 may-15_size128epoch2000
+done
+
+# using wordseg-tp
+for i in {1..5}; do
+  bash VQ_spolacq_size128_mod.bash $i tp 25 may-15_size128epoch2000
+done
+
+for i in {1..5}; do
+  bash VQ_spolacq_size128_mod.bash $i tp 36 may-15_size128epoch2000
+done
+
+
+# code size 512
+
+# using wordseg-ag
+for i in {1..5}; do
+  bash VQ_spolacq_size512_mod.bash $i ag 25 may-15_size512epoch2000
+done
+
+for i in {1..5}; do
+  bash VQ_spolacq_size512_mod.bash $i ag 36 may-15_size512epoch2000
+done
+
+# using wordseg-tp
+for i in {1..5}; do
+  bash VQ_spolacq_size512_mod.bash $i tp 25 may-15_size512epoch2000
+done
+
+for i in {1..5}; do
+  bash VQ_spolacq_size512_mod.bash $i tp 36 may-15_size512epoch2000
 done
