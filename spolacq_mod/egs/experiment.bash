48425c27efa1bf4b792a4931997f0a5fa533b89d
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-15 21:39:35 +0200

    12 to 18 percent simulated wordseg results

--- spolacq_mod/egs/experiment.bash
+++ spolacq_mod/egs/experiment.bash
@@ -1,9 +1,17 @@
 cd ../scripts/
 
-python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_90percent \
+python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_12percent \
 --num_words=500 \
---utterance=45
+--utterance=6
 
-python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_100percent \
+python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_14percent \
 --num_words=500 \
---utterance=50
+--utterance=7
+
+python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_16percent \
+--num_words=500 \
+--utterance=8
+
+python main_simulate_wordseg.py --data_name=may-15_simulatedwordseg_18percent \
+--num_words=500 \
+--utterance=9
