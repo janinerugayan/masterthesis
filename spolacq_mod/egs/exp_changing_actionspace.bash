2fc174b4b23fa07724e4b5434da175d202670aa9
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-16 15:20:54 +0200

    changed recog rate to 40 percent, and then from 200 to 1000 action space

--- spolacq_mod/egs/exp_changing_actionspace.bash
+++ spolacq_mod/egs/exp_changing_actionspace.bash
@@ -1,41 +1,37 @@
 cd ../scripts/
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace100 \
---num_words=100 \
---utterance=15
-
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace200 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace200 \
 --num_words=200 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace300 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace300 \
 --num_words=300 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace400 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace400 \
 --num_words=400 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace500 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace500 \
 --num_words=500 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace600 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace600 \
 --num_words=600 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace700 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace700 \
 --num_words=700 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace800 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace800 \
 --num_words=800 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace900 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace900 \
 --num_words=900 \
---utterance=15
+--utterance=20
 
-python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace1000 \
+python main_simulate_wordseg.py --data_name=may-16_recog40percent_simactionspace1000 \
 --num_words=1000 \
---utterance=15
+--utterance=20
