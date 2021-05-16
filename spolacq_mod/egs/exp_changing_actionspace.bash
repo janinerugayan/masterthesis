738e647b79b30582ef88d18dc31a6a1c767c259a
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-16 15:06:29 +0200

    changing action space experiment

---
+++ spolacq_mod/egs/exp_changing_actionspace.bash
@@ -0,0 +1,41 @@
+cd ../scripts/
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace100 \
+--num_words=100 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace200 \
+--num_words=200 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace300 \
+--num_words=300 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace400 \
+--num_words=400 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace500 \
+--num_words=500 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace600 \
+--num_words=600 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace700 \
+--num_words=700 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace800 \
+--num_words=800 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace900 \
+--num_words=900 \
+--utterance=15
+
+python main_simulate_wordseg.py --data_name=may-16_recog30percent_simactionspace1000 \
+--num_words=1000 \
+--utterance=15
