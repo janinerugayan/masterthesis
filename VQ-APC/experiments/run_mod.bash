4c3d2130a36fcf4c8a2b4541e365626a299e4f18
Janine Rugayan <janinelr@stud.ntnu.no>
2021-05-23 21:04:04 +0200

    commented out size 128

--- VQ-APC/experiments/run_mod.bash
+++ VQ-APC/experiments/run_mod.bash
@@ -1,12 +1,12 @@
 # code size 128
 
 # using wordseg-ag
-for i in {1..5}; do
-  bash VQ_spolacq_size128_mod.bash $i ag 25 may-23_size128epoch1000_numshuffledver2
-done
+# for i in {1..5}; do
+#   bash VQ_spolacq_size128_mod.bash $i ag 25 may-23_size128epoch1000_numshuffledver2
+# done
 
 # using wordseg-tp
-bash VQ_spolacq_size128_mod.bash 1 tp 25 may-23_size128epoch1000_numshuffledver2
+# bash VQ_spolacq_size128_mod.bash 1 tp 25 may-23_size128epoch1000_numshuffledver2
 
 
 # code size 256
