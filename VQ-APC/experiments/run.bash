0cdba885b57e7773c109047a6db2fe101e262c76
Janine Rugayan <janinelr@stud.ntnu.no>
2021-06-02 08:33:32 +0200

    size 256 epoch 1600 experiment

--- VQ-APC/experiments/run.bash
+++ VQ-APC/experiments/run.bash
@@ -1,26 +1,26 @@
 # code size 256
 # using wordseg-ag
-# for i in {1..5}; do
-#   bash VQ_spolacq_size256.bash $i ag 36 june-2_size256epoch1600
-# done
+for i in {1..5}; do
+  bash VQ_spolacq_size256.bash $i ag 36 june-2_size256epoch1600
+done
 # using wordseg-tp
-# bash VQ_spolacq_size256.bash 1 tp 36 june-2_size256epoch1600
+bash VQ_spolacq_size256.bash 1 tp 36 june-2_size256epoch1600
 
 
 # code size 128
 # using wordseg-ag
-for i in {1..5}; do
-  bash VQ_spolacq_size128.bash $i ag 36 june-2_size128epoch1600
-done
+# for i in {1..5}; do
+#   bash VQ_spolacq_size128.bash $i ag 36 june-2_size128epoch1600
+# done
 # using wordseg-tp
-bash VQ_spolacq_size128.bash 1 tp 36 june-2_size128epoch1600
+# bash VQ_spolacq_size128.bash 1 tp 36 june-2_size128epoch1600
 
 
 
 # code size 512
 # using wordseg-ag
-for i in {1..5}; do
-  bash VQ_spolacq_size512.bash $i ag 36 june-2_size512epoch1600
-done
+# for i in {1..5}; do
+#   bash VQ_spolacq_size512.bash $i ag 36 june-2_size512epoch1600
+# done
 # using wordseg-tp
-bash VQ_spolacq_size512.bash 1 tp 36 june-2_size512epoch1600
+# bash VQ_spolacq_size512.bash 1 tp 36 june-2_size512epoch1600
