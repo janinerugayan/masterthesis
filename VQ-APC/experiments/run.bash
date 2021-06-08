854a2da23f192d2a09875d34c077a6bdb6864756
Janine Rugayan <janinelr@stud.ntnu.no>
2021-06-08 17:28:44 +0200

    size 512 epoch 2000 experiment rerun

--- VQ-APC/experiments/run.bash
+++ VQ-APC/experiments/run.bash
@@ -1,10 +1,10 @@
 # code size 256
 # using wordseg-ag
-for i in {1..5}; do
-  bash VQ_spolacq_size256.bash $i ag 36 june-8_size256epoch2000
-done
+# for i in {1..5}; do
+#   bash VQ_spolacq_size256.bash $i ag 36 june-8_size256epoch2000
+# done
 # using wordseg-tp
-bash VQ_spolacq_size256.bash 1 tp 36 june-8_size256epoch2000
+# bash VQ_spolacq_size256.bash 1 tp 36 june-8_size256epoch2000
 
 
 # code size 128
@@ -19,8 +19,8 @@ bash VQ_spolacq_size256.bash 1 tp 36 june-8_size256epoch2000
 
 # code size 512
 # using wordseg-ag
-# for i in {1..5}; do
-#   bash VQ_spolacq_size512.bash $i ag 36 june-2_size512epoch1600
-# done
+for i in {1..5}; do
+  bash VQ_spolacq_size512.bash $i ag 36 june-8_size512epoch2000
+done
 # using wordseg-tp
-# bash VQ_spolacq_size512.bash 1 tp 36 june-2_size512epoch1600
+bash VQ_spolacq_size512.bash 1 tp 36 june-8_size512epoch2000
